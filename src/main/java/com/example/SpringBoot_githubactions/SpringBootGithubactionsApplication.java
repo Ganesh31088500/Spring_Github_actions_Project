c23cecaa98ed9661ffbd4effb53ccf443fa836f9
Ganesh31088500 <ganeshpitchuka8@gmail.com>
2025-08-25 08:40:20 +0530

    first commit

---
+++ src/main/java/com/example/SpringBoot_githubactions/SpringBootGithubactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGithubactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGithubactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_githubactions/SpringBootGithubactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGithubactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
